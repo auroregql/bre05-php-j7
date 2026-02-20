aaa049381b488da160da267ceacc4844350be911
auroregql <aurore.gicquel.06@gmail.com>
2026-02-20 09:59:57 +0000

    Mini projet sur php : rajout de la manip .env

--- auth/connexion.php
+++ auth/connexion.php
@@ -1,18 +1,27 @@
 <?php
-$host = "db.3wa.io";
-$port = "3306";
-$dbname = "VOTREID_php_j6";
-$connexionString = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
 
-$user = "VOTREID";
-$password = "VOTREMDP";
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Chargement des variables d'environnement
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Utilisation des variables d'environnement
+$host = $_ENV['DB_HOST'];
+$port = $_ENV['DB_PORT'];
+$user = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+$dbname = $_ENV['DB_NAME'];
+
+$connexionString = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
 
 $db = new PDO(
     $connexionString,
     $user,
     $password
-);
 
-// echo "<pre>";
-// var_dump($db);
-// echo "</pre>";
+);
+    
+var_dump($db);
+
+?>
